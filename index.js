f9d4ad0d531dfc7b460081538e4aa561b9787aa7
ShorenaK <shorenaanzhilov@gmail.com>
2023-02-23 15:39:23 -0500

    fun getFEedHtml implemnted

--- index.js
+++ index.js
@@ -7,3 +7,34 @@ tweetBtn.addEventListener('click', function(){
     console.log(tweetInput.value)
     tweetInput.value = ''
 })
+
+function getFeedHtml(){
+    let feedHmlt = ``
+    for (let tweet of tweetsData){
+    feedHmlt += `
+<div class="tweet">
+    <div class="tweet-inner">
+        <img src="${tweet.profilePic}" class="profile-pic">
+        <div>
+            <p class="handle">${tweet.handle}</p>
+            <p class="tweet-text">${tweet.tweetText}</p>
+            <div class="tweet-details">
+                <span class="tweet-detail">
+                    ${tweet.replies.length}
+                </span>
+                <span class="tweet-detail">
+                    ${tweet.likes}
+                </span>
+                <span class="tweet-detail">
+                    ${tweet.retweets}
+                </span>
+            </div>   
+        </div>            
+    </div>
+</div>`
+
+}
+return feedHmlt
+}
+
+getFeedHtml()
